246b26516624653708d8f9b8a97cd6612ddc5e03
antonryan98 <antonius.aryantoro@gmail.com>
2018-06-27 21:05:21 +0700

    first commit

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,14 @@
+package app;
+
+/**
+ * Created by Asus on 27/05/2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.8.101/loginkomik/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.8.101/loginkomik/register.php";
+}
+
